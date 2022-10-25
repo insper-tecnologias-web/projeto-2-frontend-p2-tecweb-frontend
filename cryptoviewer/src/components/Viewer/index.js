d2d303b0e857c1742814d1427ac826b99cd2d8c9
Antônio A E Martins <antonioaem@icloud.com>
2022-10-25 18:48:26 -0300

    text-principal

--- cryptoviewer/src/components/Viewer/index.js
+++ cryptoviewer/src/components/Viewer/index.js
@@ -4,8 +4,14 @@ import "./index.css";
 export default function View() {
   return (
     <div className="grid-container">
-      <div className="item1">Texto1</div>
-      <div className="item2">Texto2</div>
+      <div className="item1">
+        <div className="row">
+        <p className="text-principal">BTC/ETH</p>
+        <p className="text-principal">2.3456</p>
+        <p className="text-principal">+ 3.45%</p>
+        </div>
+      </div>
+      <div className="item2"></div>
       <div className="item3">Texto3</div>
       <div className="item4">Texto4</div>
     </div>
