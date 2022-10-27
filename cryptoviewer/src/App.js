0dc7fc497cf994b909bd5c962b256f416a488301
Arthur Martins de Souza Barreto <82684357+Arthur-Barreto@users.noreply.github.com>
2022-10-27 09:12:29 -0300

    conectando ao back

--- cryptoviewer/src/App.js
+++ cryptoviewer/src/App.js
@@ -1,10 +1,28 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import View from "./components/Viewer";
 import "./index.css";
 
 function App() {
+
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/coins")
+      .then((response) => setCoins(response.data));
+  }, []);
+
+  console.log(coins);
+
   return (
-    <div style={{height:"100vh"}}>
-      <View></View>
+    <div>
+      <div style={{height:"100vh"}}>
+        <View></View>
+      </div>
+      {/* <div> 
+        <p> {request} </p>
+      </div> */}
     </div>
   )
 }
