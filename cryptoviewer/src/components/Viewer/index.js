8fe661984cae3acc99806ff12b021028a2d7e389
Antônio A E Martins <antonioaem@icloud.com>
2022-11-01 17:41:14 -0300

    CandleChart
    Neste commit foram realizadas alterações em 4 arquivos, todas referentes a criação do Gráfico Candle.

--- cryptoviewer/src/components/Viewer/index.js
+++ cryptoviewer/src/components/Viewer/index.js
@@ -1,37 +1,7 @@
-<<<<<<< Updated upstream
-import Candle from "../Candle/candle";
-=======
-<<<<<<< Updated upstream
-import React from "react";
-=======
-import CandleChart from "../Candle/candle";
->>>>>>> Stashed changes
->>>>>>> Stashed changes
-import "./index.css";
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-
-const options = {   
-  headers: {"X-CoinAPI-Key":"43725491-667F-4F7F-A073-734061BA32CF"}
-}
-async function getData(base, quote, perio){
-// Banco de informações
-let candleData = [];
-// Axios
-const dados = await axios.get("https://rest.coinapi.io/v1/exchangerate/BTC/USD/history?period_id=1DAY&time_start=2016-01-01T00:00:00&time_end=2018-12-01T00:00:00", options)
-console.log(dados)
-// Separação das Rates:
-for (let dado in dados.data){
-    let info = dados.data[dado]
-    let timeDate = info.time_period_end.split("T")[0].split("-")
-    let rateOpen = info.rate_open
-    let rateHigh = info.rate_high
-    let rateLow = info.rate_low
-    let rateClose = info.rate_close
-    candleData.push({x: new Date(timeDate[0],timeDate[1], timeDate[2]), y:[rateOpen, rateHigh, rateLow, rateClose]})
-}
-return candleData
-}
+import CandleChart from "../Candle/candle";
+import "../Viewer/index.css";
+import axios from 'axios';
 
 const graph = document.getElementById("grafico")
 let size = 456;
